3f65cc4c8008bd96a5d077c572023759eecd1d9b
Burak Arslan <burak-github@arskom.com.tr>
2009-12-16 02:16:28 +0200

    fix whitespace

--- soaplib/soap.py
+++ soaplib/soap.py
@@ -53,7 +53,7 @@ class Message(object):
     def to_xml(self, *data):
         if len(self.params):
             if len(self.params) >= 2:
-              data = data[0]
+                data = data[0]
             if len(data) != len(self.params):
                 raise Exception("Parameter number mismatch expected [%s] "
                     "got [%s] for response %s"%(len(self.params), len(data), self.name))
