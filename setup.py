e7ee965feb4f114852b936fb083bbd2731f0eea7
Burak Arslan <burak-github@arskom.com.tr>
2010-06-21 01:17:28 +0300

    version tweak

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-VERSION = '0.9.0dev'
+VERSION = '0.9.0'
 LONG_DESC = """\
 This is a simple, easily extendible soap library that provides several useful
 tools for creating and publishing soap web services in python.  This package
