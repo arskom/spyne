a255a406129ff69e5937f4df9d37192012deda88
Burak Arslan <burak-github@arskom.com.tr>
2010-06-29 15:17:01 +0300

    header tweak

--- tests/serializers/test_binary.py
+++ tests/serializers/test_binary.py
@@ -1,6 +1,6 @@
 
 #
-# soaplib - Copyright (C) 2009 Aaron Bickell, Jamie Kirkpatrick
+# soaplib - Copyright (C) Soaplib contributors.
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
