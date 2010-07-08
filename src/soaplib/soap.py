6f5ea8f9a1cffc466bb3186a8f37a68840261081
Burak Arslan <burak-github@arskom.com.tr>
2010-07-08 13:06:34 +0300

    fixed fault ns assignment

--- src/soaplib/soap.py
+++ src/soaplib/soap.py
@@ -418,6 +418,6 @@ def make_soap_fault(tns, fault_string, fault_code = 'Server', detail = None,
     body = etree.SubElement(envelope, '{%s}Body'  % _ns_soap_env)
 
     f = Fault(fault_code, fault_string, detail)
-    body.append(Fault.to_xml(f, _ns_soap_env, "Fault"))
+    body.append(Fault.to_xml(f, tns, "Fault"))
 
     return envelope
