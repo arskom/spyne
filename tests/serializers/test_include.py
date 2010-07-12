76f3b9dd8bb5fb763dd6639ebb6fde9bbcb4c8d6
Burak Arslan <burak-github@arskom.com.tr>
2010-07-12 13:20:52 +0300

    refactored test to work new soap, also made some style adjustments

--- tests/serializers/test_include.py
+++ tests/serializers/test_include.py
@@ -1,46 +1,57 @@
-import unittest
 
-from soaplib.serializers.primitive import String, Integer
+import unittest
+from urllib import urlquote
+
+from lxml import etree
+
+import soaplib
+
 from soaplib.serializers.clazz import ClassSerializer
+from soaplib.serializers.primitive import Integer
+from soaplib.serializers.primitive import String
 from soaplib.soap import join_attachment
 
-from StringIO import StringIO
-from soaplib.xml import ElementTree
-
-
-##########################################################
 # Service Classes
-##########################################################
-
 class DownloadPartFileResult(ClassSerializer):
-    class types:
-        ErrorCode = Integer
-        ErrorMessage = String
-        Data = String
+    ErrorCode = Integer
+    ErrorMessage = String
+    Data = String
 
-##########################################################
 # Tests
-##########################################################
-
-class test(unittest.TestCase):
-
+class TestInclude(unittest.TestCase):
     def test_join_attachment(self):
-        id="http://tempuri.org/1/634133419330914808"
+        href_id="http://tempuri.org/1/634133419330914808"
         payload="ANJNSLJNDYBC SFDJNIREMX:CMKSAJN"
-        envelope = '<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"&gt;&lt;s:Body&gt;&lt;DownloadPartFileResponse xmlns="http://tempuri.org/"&gt;&lt;DownloadPartFileResult xmlns:a="http://schemas.datacontract.org/2004/07/KlanApi.Common" xmlns:i="http://www.w3.org/2001/XMLSchema-instance"&gt;&lt;a:ErrorCode&gt;0&lt;/a:ErrorCode&gt;&lt;a:ErrorMessage i:nil="true"/><a:Data><xop:Include href="cid:http%3A%2F%2Ftempuri.org%2F1%2F634133419330914808" xmlns:xop="http://www.w3.org/2004/08/xop/include"/&gt;&lt;/a:Data&gt;&lt;/DownloadPartFileResult&gt;&lt;/DownloadPartFileResponse&gt;&lt;/s:Body&gt;&lt;/s:Envelope&gt;'
-        (joinedmsg, numreplaces) = join_attachment(id, envelope, payload)
+        envelope = '''
+            <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">
+                <s:Body>
+                    <DownloadPartFileResponse xmlns="http://tempuri.org/">
+                        <DownloadPartFileResult xmlns:a="http://schemas.datacontract.org/2004/07/KlanApi.Common" xmlns:i="http://www.w3.org/2001/XMLSchema-instance">
+                            <a:ErrorCode>0</a:ErrorCode>
+                            <a:ErrorMessage i:nil="true"/>
+                            <a:Data>
+                                <xop:Include href="cid:%s" xmlns:xop="http://www.w3.org/2004/08/xop/include"/>
+                            </a:Data>
+                        </DownloadPartFileResult>
+                    </DownloadPartFileResponse>
+                </s:Body>
+            </s:Envelope>
+        ''' % urlquote(href_id)
 
-        soapmsg = StringIO(joinedmsg)
-        soaptree = ElementTree.parse(soapmsg)
+        (joinedmsg, numreplaces) = join_attachment(href_id, envelope, payload)
 
-        soapns = "http://schemas.xmlsoap.org/soap/envelope/"
-        r = DownloadPartFileResult.from_xml( soaptree.getroot().find("{%s}Body" % soapns).getchildren()[0].getchildren()[0] )
+        soaptree = etree.fromstring(joinedmsg)
+
+        body = soaptree.find("{%s}Body" % soaplib.ns_soap_env)
+        response = body.getchildren()[0]
+        result = response.getchildren()[0]
+        r = DownloadPartFileResult.from_xml(result)
 
         self.assertEquals(payload, r.Data)
 
-def test_suite():
+def suite():
     loader = unittest.TestLoader()
-    return loader.loadTestsFromTestCase(test)
+    return loader.loadTestsFromTestCase(TestInclude)
 
 if __name__== '__main__':
-    unittest.TextTestRunner().run(test_suite())
+    unittest.TextTestRunner().run(suite())
