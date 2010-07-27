3c24b5a2c14ae26f5992fc9101256b025cb8a359
Burak Arslan <burak-github@arskom.com.tr>
2010-07-27 10:33:08 +0300

    ch -> console_handler

--- tests/interopd.py
+++ tests/interopd.py
@@ -37,10 +37,10 @@ import logging
 logger = logging.getLogger('soaplib')
 logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-ch.setFormatter(formatter)
-logger.addHandler(ch)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
 
 class SimpleClass(ClassSerializer):
     i = Integer
