6896c34074bb7ff8cb649b4589797a4e5049e108
Burak Arslan <burak-github@arskom.com.tr>
2010-08-02 11:33:07 +0300

    logger configuration simplified

--- src/soaplib/test/interop/server/_service.py
+++ src/soaplib/test/interop/server/_service.py
@@ -33,13 +33,7 @@ from soaplib import service
 from soaplib.service import rpc
 
 import logging
-logger = logging.getLogger('soaplib')
-logger.setLevel(logging.DEBUG)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.DEBUG)
-console_handler.setFormatter(formatter)
-logger.addHandler(console_handler)
+logger = logging.getLogger(__name__)
 
 class SimpleClass(ClassSerializer):
     i = Integer
