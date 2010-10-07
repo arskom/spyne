7f590114e6ef0554cfcd01506047d792509d77fd
Burak Arslan <burak-github@arskom.com.tr>
2010-10-07 15:40:36 +0300

    integrate complex return interop test to suds tests

--- src/soaplib/test/interop/test_suds.py
+++ src/soaplib/test/interop/test_suds.py
@@ -196,5 +196,13 @@ class TestSuds(unittest.TestCase):
         raise Exception("test something! :)")
         # TODO: write asserts
 
+    def test_complex_return(self):
+        ret = self.client.service.complex_return()
+
+        self.assertEquals(ret.resultCode, 1)
+        self.assertEquals(ret.resultDescription, "Test")
+        self.assertEquals(ret.transactionId, 123)
+        self.assertEquals(ret.roles.role[0], "MEMBER")
+
 if __name__ == '__main__':
     unittest.main()
