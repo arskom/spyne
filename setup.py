f4e0a1de4e653d57fab94d8506b5c83596c54f0d
Burak Arslan <burak-github@arskom.com.tr>
2010-10-07 14:37:04 +0300

    tweak pypi info

--- setup.py
+++ setup.py
@@ -8,11 +8,14 @@ This is a simple, easily extendible soap library that provides several useful
 tools for creating and publishing soap web services in python.  This package
 features on-demand wsdl generation for the published services, a
 wsgi-compliant web application, support for complex class structures, binary
-attachments, simple framework for creating additional serialization mechanisms
-and a client library.
+attachments, and a simple framework for creating additional serialization 
+mechanisms.
 
-This prokect now uses lxml as it's XML API, providing full namespace support.
+This project uses lxml as it's XML API, providing full namespace support.
 """
+
+SHORT_DESC="A transport and architecture agnostic soap (de)serialization " \
+           "library that focuses on making small, rpc-like messaging work."
 
 setup(
     name='soaplib',
@@ -20,13 +23,13 @@ setup(
     package_dir={'':'src'},
 
     version=VERSION,
-    description="A simple library for writing soap web services",
+    description=SHORT_DESC,
     long_description=LONG_DESC,
     classifiers=[
         'Programming Language :: Python',
         'Operating System :: OS Independent',
         'Natural Language :: English',
-        'Development Status :: 4 - Beta',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
