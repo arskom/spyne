7cc84a327d914433a1724f5c19ad99f4ebcd003b
Burak Arslan <burak-github@arskom.com.tr>
2010-10-14 20:37:23 +0300

    doc tweak

--- src/soaplib/server/wsgi.py
+++ src/soaplib/server/wsgi.py
@@ -17,7 +17,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
 #
 
-"""A soap server that uses http as transport, and wsgi as bridge protocol"""
+"""A soap server that uses http as transport, and wsgi as bridge api"""
 
 from lxml import etree
 
