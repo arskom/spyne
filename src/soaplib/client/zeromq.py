00def3edc8ce1e6169b9492c89f0ad932333d438
Burak Arslan <burak-github@arskom.com.tr>
2010-10-14 20:37:42 +0300

    initial commit for a zeromq req/rep client.

---
+++ src/soaplib/client/zeromq.py
@@ -0,0 +1,45 @@
+
+#
+# soaplib - Copyright (C) Soaplib contributors.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+#
+
+"""A soap client that uses zeromq (zmq.REQ) as transport"""
+
+import zmq
+
+from soaplib.client import Service
+from soaplib.client import RemoteProcedureBase
+
+context = zmq.Context()
+
+class _RemoteProcedure(RemoteProcedureBase):
+    def __call__(self, *args, **kwargs):
+        out_str = self.get_out_string(args, kwargs)
+
+        socket = context.socket(zmq.REQ)
+        socket.connect (self.url)
+        socket.send (out_str)
+    
+        in_str = socket.recv()
+
+        return self.get_in_object(in_str)
+
+class Client(object):
+    def __init__(self, url, app):
+        super(Client, self).__init__(url, app)
+
+        self.service = Service(_RemoteProcedure, url, app)
