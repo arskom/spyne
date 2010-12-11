74469c49ffe3b5c416af983e8e1daa8e295d2da3
Tres Seaver <tseaver@palladion.com>
2010-12-11 08:13:20 -0500

    Faults need names, too.

--- src/soaplib/service.py
+++ src/soaplib/service.py
@@ -634,6 +634,7 @@ class DefinitionBase(object):
                     fault.set('name', f.get_type_name())
 
                     soap_fault = etree.SubElement(fault, '{%s}fault' % ns_soap)
+                    soap_fault.set('name', f.get_type_name())
                     soap_fault.set('use', 'literal')
 
             if method.is_callback:
