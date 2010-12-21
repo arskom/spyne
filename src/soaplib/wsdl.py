a9816df8b94031a9b90f28736ae50dd558361227
caustin <chris@sydneysys.com>
2010-12-21 14:14:42 -0600

    cleaning up the api.

--- src/soaplib/wsdl.py
+++ src/soaplib/wsdl.py
@@ -1,3 +1,22 @@
+
+#
+# soaplib - Copyright (C) Soaplib contributors.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+#
+
 import logging
 from lxml import etree
 
@@ -36,6 +55,7 @@ class WSDL():
                   xml_declaration=True,
                   encoding="UTF-8"
     ):
+        """Returns a string reprensation of the  wsdl as fornatted xml."""
 
         if self.elements is None:
             self.build_wsdl()
@@ -72,7 +92,7 @@ class WSDL():
         self.application.build_schema(self.application.types)
 
 
-    def _build_messages(self):
+    def build_messages(self):
         ''' Builds the wsdl:message elements '''
 
         self.application.messages = set()
@@ -276,7 +296,7 @@ class WSDL():
 
         self._build_root()
         self._build_types()
-        self._build_messages()
+        self.build_messages()
 
 
         if self._with_plink:
