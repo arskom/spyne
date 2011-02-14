8d47a3324fd90a30874f2b003ec582f4f450c689
caustin <chris@sydneysys.com>
2011-02-14 15:38:20 -0600

    removing dead code
    Signed-off-by: Burak Arslan <burak+github@arskom.com.tr>

--- src/rpclib/util/model_utils.py
+++ src/rpclib/util/model_utils.py
@@ -102,16 +102,6 @@ class ClassSerializerConverter():
 
         return self.__get_etree()
 
-#        root_element = etree.Element(self.parent_tag)
-#        self.instance.to_parent_element(self.instance, self.tns, root_element)
-#
-#        if not self.include_parent:
-#            return root_element[0]
-#        else:
-#           return root_element
-
-
-
     def to_xml(self):
         """Returns a xml string from a rpclib.model.clazz.ClassSerializer instance.
         """
