bf9ed45ffffe694bb489d867f007f0a6bf4e10e1
caustin <chris@sydneysys.com>
2011-02-15 15:38:21 -0600

    removing dead code (cherry picked from commit de9249dc6ae78753f65b49638ce12e212fd419da)
    Signed-off-by: Burak Arslan <burak+github@arskom.com.tr>

--- src/rpclib/util/model_utils.py
+++ src/rpclib/util/model_utils.py
@@ -70,10 +70,7 @@ class ClassSerializerConverter():
 
         for k in element.attrib.keys():
             if k not in ["{http://www.w3.org/2001/XMLSchema-instance}nil"]:
-                key = k
-                val = element.attrib[k]
                 new_el.attrib[k] = element.attrib[k]
-                foo = "A"
 
         for child in element.iterchildren():
             new_child = self.__get_ns_free_element(child)
