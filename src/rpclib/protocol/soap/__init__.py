1fb85c77deff69b5303bad632a771e60b47f8fc3
Burak Arslan <burak+github@arskom.com.tr>
2011-05-23 19:35:19 +0300

    fix imports

--- src/rpclib/protocol/soap/__init__.py
+++ src/rpclib/protocol/soap/__init__.py
@@ -1,4 +1,5 @@
 
-from base import Soap11
-from base import _from_soap
-from base import _parse_xml_string
+from _base import Soap11
+from _base import Soap11Strict
+from _base import _from_soap
+from _base import _parse_xml_string
