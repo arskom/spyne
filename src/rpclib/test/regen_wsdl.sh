dd332637381a273b1929d2c3a7850318109d980e
Burak Arslan <burak+github@arskom.com.tr>
2011-05-31 20:01:48 +0300

    hacked a small script to sort wsdl data. it'll make easier debugging of wsdl changes.

--- src/rpclib/test/regen_wsdl.sh
+++ src/rpclib/test/regen_wsdl.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-wget localhost:9753/?wsdl -qO - | tidy -xml -indent -wrap 0 > wsdl.xml 
+wget localhost:9753/?wsdl -qO - | ./sort_nodes.py | tidy -xml -indent -wrap 0 --sort-attributes alpha | sed -e "s/ xmlns:/\n                  xmlns:/g" > wsdl.xml 
 
