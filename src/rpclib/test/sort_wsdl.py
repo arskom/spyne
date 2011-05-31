e24bbe5f86723ee4b7b9c653548babc217d0dcd8
Burak Arslan <burak+github@arskom.com.tr>
2011-05-31 20:15:00 +0300

    fix indentation inconsistencies

--- src/rpclib/test/sort_wsdl.py
+++ src/rpclib/test/sort_wsdl.py
@@ -51,42 +51,41 @@ def main():
 
     l0.sort(key=lambda e: (wsdl_order[e.tag], e.attrib['name']))
     for e in l0:
-            tree.getroot().append(e)
+        tree.getroot().append(e)
 
     for e in tree.getroot():
-            if e.tag in ("{%s}portType" % ns_wsdl, "{%s}binding" % ns_wsdl):
-                    nodes = []
-                    for p in e.getchildren():
-                            nodes.append(p)
+        if e.tag in ("{%s}portType" % ns_wsdl, "{%s}binding" % ns_wsdl):
+            nodes = []
+            for p in e.getchildren():
+                nodes.append(p)
 
-                    nodes.sort(key=lambda e: e.attrib.get('name','0'))
+            nodes.sort(key=lambda e: e.attrib.get('name','0'))
 
-                    for p in nodes:
-                            e.append(p)
+            for p in nodes:
+                e.append(p)
 
 
     schemas = []
 
     for e in type_node:
-            schemas.append(e)
-            e.getparent().remove(e)
+        schemas.append(e)
+        e.getparent().remove(e)
 
     schemas.sort(key=lambda e: e.attrib["targetNamespace"])
 
     for s in schemas:
-            type_node.append(s)
+        type_node.append(s)
 
     for s in schemas:
-            nodes = []
-            for e in s:
-                    nodes.append(e)
-                    e.getparent().remove(e)
-                    #print e, e.tag, e.attrib
-            #print
-            nodes.sort(key=lambda e: (schema_order[e.tag],))
+        nodes = []
+        for e in s:
+            nodes.append(e)
+            e.getparent().remove(e)
 
-            for e in nodes:
-                    s.append(e)
+        nodes.sort(key=lambda e: (schema_order[e.tag],))
+
+        for e in nodes:
+            s.append(e)
 
     print etree.tostring(tree)
 
