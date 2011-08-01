afa71ea5c354d3f4c33c5bc6a237ab32863af593
Burak Arslan <burak.arslan@arskom.com.tr>
2011-08-01 20:32:08 +0300

    Revert "simplify multiple header parsing"
    This reverts commit 613997925bd5f4c13912e255a9cd09bd4011543a.

--- src/rpclib/interface/wsdl.py
+++ src/rpclib/interface/wsdl.py
@@ -516,15 +516,21 @@ class Wsdl11(Base):
                 in_header = service.__in_header__
 
             if not (in_header is None):
-                if not isinstance(in_header, (list, tuple)):
-                    in_header = (in_header,)
+                if isinstance(in_header, (list, tuple)):
+                    in_headers = in_header
+                    in_header_message_name = ''.join((method.name,
+                                                      _in_header_msg_suffix))
+                    in_header_pref = pref_tns
+                else:
+                    in_headers = (in_header,)
+                    in_header_message_name = in_header.get_type_name()
+                    in_header_pref = in_header.get_namespace_prefix(self)
 
-                for header in in_header:
+                for header in in_headers:
                     soap_header = etree.SubElement(input, '{%s}header' % _ns_soap)
                     soap_header.set('use', 'literal')
-                    soap_header.set('message', '%s:%s' % (
-                                            header.get_namespace_prefix(self),
-                                            header.header.get_type_name()))
+                    soap_header.set('message', '%s:%s' % (in_header_pref,
+                                                          in_header_message_name))
                     soap_header.set('part', header.get_type_name())
 
             if not (method.is_async or method.is_callback):
@@ -540,16 +546,23 @@ class Wsdl11(Base):
                     out_header = service.__out_header__
 
                 if not (out_header is None):
-                    if not isinstance(out_header, (list, tuple)):
-                        out_header = (out_header,)
+                    if isinstance(out_header, (list, tuple)):
+                        out_headers = out_header
+                        out_header_message_name = ''.join((method.name,
+                                                         _out_header_msg_suffix))
+                        out_header_pref = pref_tns
 
-                    for header in out_header:
+                    else:
+                        out_headers = (out_header,)
+                        out_header_message_name = out_header.get_type_name()
+                        out_header_pref = out_header.get_namespace_prefix(self)
+
+                    for header in out_headers:
                         soap_header = etree.SubElement(output, '{%s}header' %
-                                                                       _ns_soap)
+                                                                    _ns_soap)
                         soap_header.set('use', 'literal')
-                        soap_header.set('message', '%s:%s' % (
-                                        out_header.get_namespace_prefix(self),
-                                        out_header.get_type_name()))
+                        soap_header.set('message', '%s:%s' % (out_header_pref,
+                                                       out_header_message_name))
                         soap_header.set('part', header.get_type_name())
 
                     for f in method.faults:
