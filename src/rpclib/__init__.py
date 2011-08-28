b08712fa394103425284ffc4128b5d53eb5c4cda
Burak Arslan <burak.arslan@arskom.com.tr>
2011-08-29 02:43:06 +0300

    moved version information inside the module itself.

--- src/rpclib/__init__.py
+++ src/rpclib/__init__.py
@@ -16,3 +16,5 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
 #
+
+__version__ = '2.2.0-alpha'
