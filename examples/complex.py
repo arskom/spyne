dba7fd91b0c14dfedd1dc76ed2ab86b28132702c
Burak Arslan <burak.arslan@arskom.com.tr>
2011-09-05 01:41:02 +0300

    fix example license headers.

--- examples/complex.py
+++ examples/complex.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# encoding: utf8
 #
-# rpclib - Copyright (C) 2009 Aaron Bickell, Jamie Kirkpatrick
+# rpclib - Copyright (C) Rpclib contributors
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
--- examples/hellotwisted.py
+++ examples/hellotwisted.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
+# encoding: utf8
 #
-# rpclib - Copyright (C) 2009 Rpclib contributors
+# rpclib - Copyright (C) Rpclib contributors
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
