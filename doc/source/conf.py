4967b0446f263fdff253739a1718b03c8bee2743
Burak Arslan <burak.arslan@arskom.com.tr>
2011-09-12 14:22:42 +0300

    use sphinxtogithub extension so the resulting directories don't have _ prefix

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -25,7 +25,7 @@ import sys, os, rpclib
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode', 'sphinxtogithub']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['.templates']
