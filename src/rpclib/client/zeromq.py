9c14f8a6f17a989ff0625e87b8c8d9cf1b4754c8
Burak Arslan <burak+github@arskom.com.tr>
2011-09-26 00:01:11 +0300

    update client.zeromq module docstring

--- src/rpclib/client/zeromq.py
+++ src/rpclib/client/zeromq.py
@@ -17,7 +17,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
 #
 
-"""A soap client that uses ZeroMQ (zmq.REQ) as transport"""
+"""The ZeroMQ (zmq.REQ) client transport."""
 
 import zmq
 
