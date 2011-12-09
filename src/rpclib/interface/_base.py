0c14d7750f6c1d576b498275660a6c57ede904eb
Burak Arslan <burak.arslan@arskom.com.tr>
2011-12-09 16:11:52 +0200

    fix docstring typos

--- src/rpclib/interface/_base.py
+++ src/rpclib/interface/_base.py
@@ -263,8 +263,8 @@ class InterfaceBase(object):
 
     def get_interface_document(self, cls):
         """This function is called by server transports that try to satisfy the
-        request for the interface document. This should just return previously-cached
-        interface document.
+        request for the interface document. This should just return previously
+        cached interface document.
         """
 
         raise NotImplementedError('Extend and override.')
