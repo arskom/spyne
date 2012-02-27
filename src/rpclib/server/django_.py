ea9ff9e7db77ef5ea74e8798eb102a03feee1c0d
Burak Arslan <burak.arslan@arskom.com.tr>
2012-02-27 22:23:02 +0200

    probably fix the relative/absolute import within the django server module.

--- src/rpclib/server/django_.py
+++ src/rpclib/server/django_.py
@@ -19,4 +19,26 @@
 
 from __future__ import absolute_import
 
-from rpclib.server.django_ import DjangoApplication
+from django.http import HttpResponse
+from rpclib.server.wsgi import WsgiApplication
+
+class DjangoApplication(WsgiApplication):
+    def __call__(self, request):
+        django_response = HttpResponse()
+
+        def start_response(status, headers):
+            status, reason = status.split(' ', 1)
+
+            django_response.status_code = int(status)
+            for header, value in headers:
+                django_response[header] = value
+
+        environ = request.META.copy()
+        environ['wsgi.input'] = request
+        environ['wsgi.multithread'] = False
+
+        response = WsgiApplication.__call__(self, environ, start_response)
+
+        django_response.content = "\n".join(response)
+
+        return django_response
