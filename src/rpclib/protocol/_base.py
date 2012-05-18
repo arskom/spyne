1d4795c634d02e168e8bc96f201466752abe8fa3
Burak Arslan <burak.arslan@arskom.com.tr>
2012-05-19 01:45:21 +0300

    pep8 tweak

--- src/rpclib/protocol/_base.py
+++ src/rpclib/protocol/_base.py
@@ -188,7 +188,8 @@ class ProtocolBase(object):
     def generate_method_contexts(self, ctx):
         call_handles = self.get_call_handles(ctx)
         if len(call_handles) == 0:
-            raise ResourceNotFoundError('Method %r not found.' % ctx.method_request_string)
+            raise ResourceNotFoundError('Method %r not found.' %
+                                                      ctx.method_request_string)
 
         retval = []
         for sc, d in call_handles:
