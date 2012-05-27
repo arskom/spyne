387633430fa617261215eafa1b928b6e3923cc5c
Burak Arslan <burak.arslan@arskom.com.tr>
2012-05-27 13:45:07 +0300

    remove needless exception reference.

--- src/rpclib/util/oset/__init__.py
+++ src/rpclib/util/oset/__init__.py
@@ -1,5 +1,5 @@
 
 try:
     from rpclib.util.oset.new import oset
-except (SyntaxError, ImportError, AttributeError), e:
+except (SyntaxError, ImportError, AttributeError):
     from rpclib.util.oset.old import oset
