e6dcec780d23bd7b606b28275ae388cb2298f3c1
Burak Arslan <burak.arslan@arskom.com.tr>
2012-05-27 11:41:06 +0300

    test import cleanup

--- src/rpclib/test/protocol/test_xml.py
+++ src/rpclib/test/protocol/test_xml.py
@@ -17,25 +17,13 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
 #
 
-import json
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
 import unittest
 
-from rpclib import MethodContext
-from rpclib.application import Application
-from rpclib.decorator import srpc
-from rpclib.model.primitive import Integer
 from rpclib.model.primitive import Unicode
-from rpclib.model.primitive import DateTime
-from rpclib.model.primitive import Mandatory
 from rpclib.model.complex import ComplexModel
-from rpclib.model.complex import Iterable
-from rpclib.interface.wsdl import Wsdl11
-from rpclib.protocol.json import JsonObject
-from rpclib.service import ServiceBase
-from rpclib.server import ServerBase
 
 from rpclib.util.xml import get_xml_as_object
 from lxml import etree
