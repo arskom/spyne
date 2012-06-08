301a543d92382ceae254bb16978b8f5a20b4bcde
Burak Arslan <burak.arslan@arskom.com.tr>
2012-06-08 19:43:46 +0300

    move common py.test variables to its own variable

--- rpclib/test/run_tests.sh
+++ rpclib/test/run_tests.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -x
 
-py.test -v test_* interface/* model/* protocol/* --tb=short
+PYTEST_ARGS="-v --tb=short"
 
-py.test -v interop/test_httprpc.py
-py.test -v interop/test_soap_client_http.py
+py.test $PYTEST_ARGS test_* interface/* model/* protocol/*
+py.test $PYTEST_ARGS interop/test_httprpc.py
+py.test $PYTEST_ARGS interop/test_soap_client_http.py
+py.test $PYTEST_ARGS interop/test_soap_client_zeromq.py
+py.test $PYTEST_ARGS interop/test_suds.py
 trial      interop/test_soap_client_http_twisted.py
-py.test -v interop/test_soap_client_zeromq.py
-py.test -v interop/test_suds.py
 
 cd interop
 ./test_wsi.py
