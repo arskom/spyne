9c902cbfd4a539b4fc727256ae3091e8c7e56459
Burak Arslan <burak.arslan@arskom.com.tr>
2012-06-25 01:03:40 +0300

    makedoc.sh: fail on error

--- makedoc.sh
+++ makedoc.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-make clean -C doc
-make html -C doc
+make clean -C doc || exit 1
+make html -C doc || exit 1
 
-git checkout gh-pages
+git checkout gh-pages || exit 1
 mv .git ..
 rsync -aP doc/build/html/ ./ --delete-after
 mv ../.git .
