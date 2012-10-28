623ce3301e868191cb4460874fe1e94bfe39c819
Burak Arslan <burak.arslan@arskom.com.tr>
2012-10-28 18:40:39 +0200

    add pyparsing to test requirements

--- setup.py
+++ setup.py
@@ -114,7 +114,7 @@ class RunTests(TestCommand):
 
         raise SystemExit(ret)
 
-test_reqs = ['pytest', 'werkzeug', 'sqlalchemy', 'suds', 'msgpack-python']
+test_reqs = ['pytest', 'werkzeug', 'sqlalchemy', 'suds', 'msgpack-python', 'pyparsing']
 
 if sys.version_info < (2,6):
     test_reqs.extend([
