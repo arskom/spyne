8850d83605798fbc5a516b86df12185e4c14ed94
Burak Arslan <burak.arslan@arskom.com.tr>
2012-12-17 14:17:24 +0200

    add docstring to binary soap example

---
+++ examples/file_manager/suds_client.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from suds.client import Client
+import base64
+
+# Suds does not support base64binary type, so we do the encoding manually.
+file_data = base64.b64encode('file_data')
+
+c=Client('http://localhost:9000/filemgr/?wsdl')
+c.service.add('x', 'y', 'file_name', file_data)
+
+print 'file written.'
