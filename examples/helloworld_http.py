f5baa34c4310edf2895a7971d1266bdd44a9ae05
Erik Meusel <em.opensource@googlemail.com>
2013-01-05 23:00:34 +0100

    fixed according to documentation

--- examples/helloworld_http.py
+++ examples/helloworld_http.py
@@ -53,7 +53,7 @@ import logging
 
 from spyne.application import Application
 from spyne.decorator import srpc
-from spyne.protocol.json import JsonDocument
+from spyne.protocol.xml import XmlDocument
 from spyne.protocol.http import HttpRpc
 from spyne.service import ServiceBase
 from spyne.model.complex import Iterable
@@ -82,7 +82,7 @@ if __name__=='__main__':
     logging.basicConfig(level=logging.DEBUG)
 
     application = Application([HelloWorldService], 'spyne.examples.hello.http',
-                                in_protocol=HttpRpc(), out_protocol=JsonDocument())
+                                in_protocol=HttpRpc(), out_protocol=XmlDocument())
 
     server = make_server('127.0.0.1', 7789, WsgiApplication(application))
 
