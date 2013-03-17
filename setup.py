a1f3a9be9251f9c9e7d925588c4de47460b841cb
Burak Arslan <burak.arslan@arskom.com.tr>
2013-03-16 17:32:45 -0700

    add yaml to keywords

--- setup.py
+++ setup.py
@@ -160,7 +160,7 @@ setup(
     ],
     keywords=('soap', 'wsdl', 'wsgi', 'zeromq', 'rest', 'rpc', 'json', 'http',
               'msgpack', 'xml', 'django', 'pyramid', 'postgresql', 'sqlalchemy'
-              'werkzeug', 'twisted'),
+              'werkzeug', 'twisted', 'yaml'),
     author='Burak Arslan',
     author_email='burak+spyne@arskom.com.tr',
     maintainer='Burak Arslan',
