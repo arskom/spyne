e636a3618d938acbe061cbcf633b379b09c12f0c
Burak Arslan <burak.arslan@arskom.com.tr>
2013-03-29 12:45:14 +0200

    add gendoc.sh

---
+++ doc/gendoc.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -x
+
+rm -rf docs;
+declare -A docs=( [2.9]=2_9 [2.10]=master);
+stable=2.9
+
+for i in ${!docs[@]}; do
+    git checkout ${docs[$i]} || exit 1;
+
+    rm -rf $i;
+    make html || exit 1;
+    mv build/html $i;
+done;
+
+mkdir docs;
+mv ${!docs[@]} docs
+rsync -aP docs/$stable/ docs/
