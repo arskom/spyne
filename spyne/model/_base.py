5fa5fa9173fe6b78ffd6a22f022b89eb853726b2
Burak Arslan <burak.arslan@arskom.com.tr>
2013-07-18 20:33:28 +0100

    make onupdate and server_default arguments to customize() go to sqla_column_args

--- spyne/model/_base.py
+++ spyne/model/_base.py
@@ -357,14 +357,14 @@ class ModelBase(object):
             elif k in ('primary_key','pk'):
                 Attributes.sqla_column_args[-1]['primary_key'] = v
 
-            elif k in ('autoincrement'):
-                Attributes.sqla_column_args[-1][k] = v
-
             elif k in ('foreign_key','fk'):
                 from sqlalchemy.schema import ForeignKey
                 t, d = Attributes.sqla_column_args
                 fkt = (ForeignKey(v),)
                 Attributes.sqla_column_args = (t + fkt, d)
+
+            elif k in ('autoincrement', 'onupdate', 'server_default'):
+                Attributes.sqla_column_args[-1][k] = v
 
             elif k == 'max_occurs' and v == 'unbounded':
                 setattr(Attributes, k, Decimal('inf'))
