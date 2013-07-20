5598521e9cce72d60e6b366d8cf769396043719b
Burak Arslan <burak.arslan@arskom.com.tr>
2013-07-20 02:11:00 +0100

    update jenkins script to set up postgres database.

--- spyne/test/jenkins.sh
+++ spyne/test/jenkins.sh
@@ -9,6 +9,11 @@ fi;
 
 source _ve/bin/activate
 easy_install coverage
+
+# set up postgres
+if [ $(psql -tAlU postgres | cut  -d "|" -f1 | grep -w spyne_test_$USER | wc -l) -eq 1 ]; then
+    psql -c "drop database spyne_test_$USER" -U postgres
+fi;
 psql -c "create database spyne_test_$USER" -U postgres
 
 bash -c "coverage run setup.py test; exit 0"
