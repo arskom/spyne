146035ce9b78d4b59dcc86562ddca511f671c78c
Burak Arslan <burak.arslan@arskom.com.tr>
2013-10-20 16:21:23 +0300

    it was a bad idea to have an optional colorama.init() on spyne import.

--- spyne/__init__.py
+++ spyne/__init__.py
@@ -41,10 +41,3 @@ if not hasattr(sys, "version_info") or sys.version_info < (2, 6):
     raise RuntimeError("Spyne requires Python 2.6 or later.")
 
 del sys
-
-try:
-    import colorama
-    colorama.init()
-    del colorama
-except ImportError:
-    pass
