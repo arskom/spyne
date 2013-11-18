79252a10a326032d82e7aec46812f22f38e09a60
Burak Arslan <burak.arslan@arskom.com.tr>
2013-11-18 15:33:15 +0200

    support for separating client exceptions from server ones. #314 #284

--- spyne/application.py
+++ spyne/application.py
@@ -20,6 +20,7 @@
 
 import logging
 logger = logging.getLogger(__name__)
+logger_client = logging.getLogger('.'.join([__name__, 'client']))
 
 from spyne import BODY_STYLE_WRAPPED
 from spyne.model.fault import Fault
@@ -152,7 +153,7 @@ class Application(object):
                                                     'method_return_object', ctx)
 
         except Fault, e:
-            logger.exception(e)
+            logger_client.exception(e)
 
             ctx.out_error = e
 
