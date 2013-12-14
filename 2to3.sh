b1216ae6f8e55f5bf8b70a984962c5718e506d76
Burak Arslan <burak.arslan@arskom.com.tr>
2013-12-14 15:39:49 +0200

    begone, 2to3.sh!  #113

--- 2to3.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -x
-
-2to3 -f next -w spyne/protocol/soap/soap11.py
-2to3 -f except -f funcattrs -f metaclass -f print -w spyne
-2to3 -f except -f funcattrs -f metaclass -f print -w examples
-
