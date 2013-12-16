b69497af5893e7f84b91073ad85b6354d774a64f
Burak Arslan <burak.arslan@arskom.com.tr>
2013-12-16 14:01:03 +0200

    make run_tests.sh to bootstrap the testing environment

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,7 @@
 #!/bin/bash -x
 #
 # Requirements:
-#   1. easy_install executables of various Python installations accesible with
-#      a Python version suffix. E.g. the easy_install-2.7 command should call
-#      the easy_install script of a Python 2.7 environment.
-#   2. A working Postgresql>=9.2 installation with 'trust' authentication.
+#   1. A working build environment
 #
 # Usage:
 #   Run it like this:
@@ -25,12 +22,41 @@
 #
 
 [ -z "$PYVER" ] && PYVER=2.7
+[ -z "$WORKSPACE" ] && WORKSPACE="$PWD"
 
-if [ -z "$(which easy_install-$PYVER)" ]; then
-  echo "missing easy_install-$PYVER executable. aborting.";
-  exit 1;
+# Set up python
+
+if   [ $PYVER == "2.6" ]; then
+    FN=2.6.9/Python-2.6.9.tgz;
+
+elif [ $PYVER == "2.7" ]; then
+    FN=2.7.6/Python-2.7.6.tgz;
+
+elif [ $PYVER == "3.3" ]; then
+    FN=3.3.3/Python-3.3.3.tgz;
+
+else
+    echo "Unknown python version $PYVER";
+    exit 2;
+
 fi;
 
+PREFIX="$(basename $FN .tgz)";
+
+if [ -z "$(which easy_install-$PYVER)" ]; then
+    wget -ct0 http://www.python.org/ftp/python/$FN;
+    tar xf $(basename $FN);
+    cd "$PREFIX";
+    ./configure --prefix="$WORKSPACE/$PREFIX";
+    make -j2 && make install;
+fi;
+
+# Set up distribute
+export PATH="$WORKSPACE/$PREFIX/bin:$PATH";
+wget -ct0 http://python-distribute.org/distribute_setup.py
+python$PYVER distribute_setup.py
+
+# Run tests
 echo pyver: $PYVER
 
 easy_install-$PYVER -U --user virtualenv
@@ -44,4 +70,3 @@ easy_install coverage
 
 bash -c "coverage run --source=spyne setup.py test; exit 0"
 coverage xml -i --omit=../spyne/test/*;
-
