763b38392f66ecfcb81df2a23d26331e195b9f34
Burak Arslan <burak.arslan@arskom.com.tr>
2013-12-18 00:29:52 +0200

    s.i.x.model: removed redundant import, refactored accordingly.

--- spyne/interface/xml_schema/model.py
+++ spyne/interface/xml_schema/model.py
@@ -30,7 +30,6 @@ from lxml import etree
 from collections import deque
 from collections import defaultdict
 
-from spyne.const.xml_ns import xsd as _ns_xs
 from spyne.const.xml_ns import xsd as _ns_xsd
 
 from spyne.model.complex import XmlAttribute
@@ -272,21 +271,21 @@ def unicode_get_restriction_tag(interface, cls):
 
     # length
     if cls.Attributes.min_len == cls.Attributes.max_len:
-        length = etree.SubElement(restriction, '{%s}length' % _ns_xs)
+        length = etree.SubElement(restriction, '{%s}length' % _ns_xsd)
         length.set('value', str(cls.Attributes.min_len))
 
     else:
         if cls.Attributes.min_len != Unicode.Attributes.min_len:
-            min_l = etree.SubElement(restriction, '{%s}minLength' % _ns_xs)
+            min_l = etree.SubElement(restriction, '{%s}minLength' % _ns_xsd)
             min_l.set('value', str(cls.Attributes.min_len))
 
         if cls.Attributes.max_len != Unicode.Attributes.max_len:
-            max_l = etree.SubElement(restriction, '{%s}maxLength' % _ns_xs)
+            max_l = etree.SubElement(restriction, '{%s}maxLength' % _ns_xsd)
             max_l.set('value', str(cls.Attributes.max_len))
 
     # pattern
     if cls.Attributes.pattern != Unicode.Attributes.pattern:
-        pattern = etree.SubElement(restriction, '{%s}pattern' % _ns_xs)
+        pattern = etree.SubElement(restriction, '{%s}pattern' % _ns_xsd)
         pattern.set('value', cls.Attributes.pattern)
 
     return restriction
@@ -304,12 +303,12 @@ def Tget_range_restriction_tag(T):
     if issubclass(T, Decimal):
         def _get_float_restrictions(prot, restriction, cls):
             if cls.Attributes.fraction_digits != T.Attributes.fraction_digits:
-                elt = etree.SubElement(restriction, '{%s}fractionDigits' % _ns_xs)
+                elt = etree.SubElement(restriction, '{%s}fractionDigits' % _ns_xsd)
                 elt.set('value', prot.to_string(cls, cls.Attributes.fraction_digits))
 
         def _get_integer_restrictions(prot, restriction, cls):
             if cls.Attributes.total_digits != T.Attributes.total_digits:
-                elt = etree.SubElement(restriction, '{%s}totalDigits' % _ns_xs)
+                elt = etree.SubElement(restriction, '{%s}totalDigits' % _ns_xsd)
                 elt.set('value', prot.to_string(cls, cls.Attributes.total_digits))
 
         if issubclass(T, Integer):
@@ -330,23 +329,23 @@ def Tget_range_restriction_tag(T):
         restriction = simple_get_restriction_tag(document, cls)
 
         if cls.Attributes.gt != T.Attributes.gt:
-            elt = etree.SubElement(restriction, '{%s}minExclusive' % _ns_xs)
+            elt = etree.SubElement(restriction, '{%s}minExclusive' % _ns_xsd)
             elt.set('value', prot.to_string(cls, cls.Attributes.gt))
 
         if cls.Attributes.ge != T.Attributes.ge:
-            elt = etree.SubElement(restriction, '{%s}minInclusive' % _ns_xs)
+            elt = etree.SubElement(restriction, '{%s}minInclusive' % _ns_xsd)
             elt.set('value', prot.to_string(cls, cls.Attributes.ge))
 
         if cls.Attributes.lt != T.Attributes.lt:
-            elt = etree.SubElement(restriction, '{%s}maxExclusive' % _ns_xs)
+            elt = etree.SubElement(restriction, '{%s}maxExclusive' % _ns_xsd)
             elt.set('value', prot.to_string(cls, cls.Attributes.lt))
 
         if cls.Attributes.le != T.Attributes.le:
-            elt = etree.SubElement(restriction, '{%s}maxInclusive' % _ns_xs)
+            elt = etree.SubElement(restriction, '{%s}maxInclusive' % _ns_xsd)
             elt.set('value', prot.to_string(cls, cls.Attributes.le))
 
         if cls.Attributes.pattern != T.Attributes.pattern:
-            elt = etree.SubElement(restriction, '{%s}pattern' % _ns_xs)
+            elt = etree.SubElement(restriction, '{%s}pattern' % _ns_xsd)
             elt.set('value', cls.Attributes.pattern)
 
         _get_additional_restrictions(prot, restriction, cls)
