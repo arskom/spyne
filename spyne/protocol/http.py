36c9ec69f4e542088a077ce61e9c7db3ffe09842
Burak Arslan <burak.arslan@arskom.com.tr>
2013-12-19 16:24:38 +0200

    ignore_uncap argument to HttpRpc is now by default False

--- spyne/protocol/http.py
+++ spyne/protocol/http.py
@@ -115,7 +115,7 @@ class HttpRpc(SimpleDictDocument):
     type.add('http')
 
     def __init__(self, app=None, validator=None, mime_type=None,
-                    tmp_dir=None, tmp_delete_on_close=True, ignore_uncap=True,
+                    tmp_dir=None, tmp_delete_on_close=True, ignore_uncap=False,
                                                             parse_cookie=True):
         super(HttpRpc, self).__init__(app, validator, mime_type,
                                                       ignore_uncap=ignore_uncap)
