2ee5783568482e8d3da2aff59c057f0cce94f169
Lars van de Kerkhof <lars@permanentmarkers.nl>
2014-01-06 14:50:28 +0100

    email_re nolonger exists in django 1.6

--- spyne/util/django.py
+++ spyne/util/django.py
@@ -28,11 +28,19 @@ import logging
 logger = logging.getLogger(__name__)
 
 from django.core.exceptions import ImproperlyConfigured
-from django.core.validators import (email_re, slug_re,
+from django.core.validators import (slug_re,
                                     comma_separated_int_list_re, URLValidator)
 from spyne.model.complex import ComplexModelMeta, ComplexModelBase
 from spyne.model import primitive
 from spyne.util.six import add_metaclass
+
+
+email_re = re.compile(
+    r"(^[-!#$%&'*+/=?^_`{}|~0-9A-Z]+(\.[-!#$%&'*+/=?^_`{}|~0-9A-Z]+)*"  # dot-atom
+    # quoted-string, see also http://tools.ietf.org/html/rfc2822#section-3.2.5
+    r'|^"([\001-\010\013\014\016-\037!#-\[\]-\177]|\\[\001-\011\013\014\016-\177])*"'
+    r')@((?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)$)'  # domain
+    r'|\[(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}\]$', re.IGNORECASE)  # literal form, ipv4 address (SMTP 4.1.3)
 
 
 class DjangoFieldMapper(object):
