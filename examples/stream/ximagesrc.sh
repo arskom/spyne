e919f8102c6bcbda05a935d340db5271697232e9
Burak Arslan <burak.arslan@arskom.com.tr>
2014-04-05 18:14:43 +0300

    this is not how we're doing things anymore.

--- examples/stream/ximagesrc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# quick way to test gstreamer pipeline
-
-gst-launch-0.10 ximagesrc ! video/x-raw-rgb,framerate=2/1 ! ffmpegcolorspace \
-    ! theoraenc quality=32 ! oggmux name=mux ! filesink location=streamfile.ogg
