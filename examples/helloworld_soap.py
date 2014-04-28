e87d7318c0200551b32fab7ed77ffd32152ae1cb
Burak Arslan <burak.arslan@arskom.com.tr>
2014-04-29 00:11:34 +0300

    minor

--- examples/helloworld_soap.py
+++ examples/helloworld_soap.py
@@ -60,22 +60,21 @@ from spyne.server.wsgi import WsgiApplication
 
 from spyne.decorator import rpc
 from spyne.service import ServiceBase
-from spyne.model.complex import Iterable
-from spyne.model.primitive import Integer
-from spyne.model.primitive import Unicode
+from spyne.model import Iterable
+from spyne.model import Integer
+from spyne.model import Unicode
 
 
 class HelloWorldService(ServiceBase):
     @rpc(Unicode, Integer, _returns=Iterable(Unicode))
     def say_hello(ctx, name, times):
-        '''
-        Docstrings for service methods appear as documentation in the wsdl.
+        """Docstrings for service methods appear as documentation in the wsdl.
         <b>What fun!</b>
 
         @param name the name to say hello to
-        @param the number of times to say hello
+        @param times the number of times to say hello
         @return the completed array
-        '''
+        """
 
         for i in range(times):
             yield u'Hello, %s' % name
