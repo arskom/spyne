f83dd0f1561d19b8a1ef10b7a0c344423c8068cf
Burak Arslan <burak.arslan@arskom.com.tr>
2014-05-05 16:04:53 +0300

    update spyne.util.meta docstring. #343

--- spyne/util/meta.py
+++ spyne/util/meta.py
@@ -17,7 +17,9 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
 #
 
-"""Metaclass utilities."""
+"""Metaclass utilities for
+:attr:`spyne.model.complex.ComplexModelBase.Attributes.declare_order`
+"""
 
 import sys
 import inspect
