e2fc3f84e95e92fbe9eac493aa15f15541fe9f2f
Burak Arslan <burak.arslan@arskom.com.tr>
2014-06-27 13:43:31 +0300

    minor refactor

--- spyne/protocol/_base.py
+++ spyne/protocol/_base.py
@@ -708,34 +708,32 @@ class ProtocolBase(object):
             return binary_encoding_handlers[encoding](value)
 
     def file_to_string_iterable(self, cls, value):
-        if value.data is None:
-            if value.handle is None:
-                assert value.path is not None, "You need to write data to " \
-                         "persistent storage first if you want to read it back."
-
-                try:
-                    path = value.path
-                    if not isabs(value.path):
-                        path = join(value.store, value.path)
-                    f = open(path, 'rb')
-                except IOError as e:
-                    if e.errno == errno.ENOENT:
-                        raise ResourceNotFoundError(value.path)
-                    else:
-                        raise InternalError("Error accessing requested file")
-
-            else:
-                f = value.handle
-                f.seek(0)
-
-            return _file_to_iter(f)
-
-        else:
-            if isinstance(value.data, (list,tuple)) and \
-                                                isinstance(value.data[0], mmap):
+        if value.data is not None:
+            if isinstance(value.data, (list, tuple)) and \
+                    isinstance(value.data[0], mmap):
                 return _file_to_iter(value.data[0])
             else:
                 return iter(value.data)
+
+        if value.handle is not None:
+            f = value.handle
+            f.seek(0)
+            return _file_to_iter(f)
+
+        assert value.path is not None, "You need to write data to " \
+                 "persistent storage first if you want to read it back."
+
+        try:
+            path = value.path
+            if not isabs(value.path):
+                path = join(value.store, value.path)
+            return _file_to_iter(open(path, 'rb'))
+
+        except IOError as e:
+            if e.errno == errno.ENOENT:
+                raise ResourceNotFoundError(value.path)
+            else:
+                raise InternalError("Error accessing requested file")
 
     def simple_model_to_string_iterable(self, cls, value):
         retval = self.to_string(cls, value)
