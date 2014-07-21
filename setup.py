ae25613bbf3d0962c948ee016eae7d6ab4962fe0
Kamil Gałuszka <galuszkak@gmail.com>
2014-07-21 12:32:12 +0200

    new test structure

--- setup.py
+++ setup.py
@@ -6,6 +6,11 @@ from __future__ import print_function
 import os
 import re
 import sys
+import inspect
+
+from os.path import join, dirname, abspath
+from glob import glob
+from itertools import chain
 
 from setuptools import setup
 from setuptools import find_packages
@@ -23,8 +28,6 @@ except ImportError:
     GREEN = ''
     RED = ''
 
-import inspect
-from os.path import join, dirname, abspath
 OWN_PATH = abspath(inspect.getfile(inspect.currentframe()))
 EXAMPLES_DIR = join(dirname(OWN_PATH), 'examples')
 
@@ -52,8 +55,6 @@ except OSError:
 
 def call_test(f, a, tests):
     import spyne.test
-    from glob import glob
-    from itertools import chain
     from multiprocessing import Process, Queue
 
     tests_dir = os.path.dirname(spyne.test.__file__)
@@ -77,19 +78,18 @@ def _wrapper(f):
     def _(args, queue):
         try:
             retval = f(args)
-        except TypeError: # it's a pain to call trial.
+        except TypeError:  # it's a pain to call trial.
             sys.argv = ['trial']
             sys.argv.extend(args)
             retval = f()
         queue.put(retval)
+
     return _
 
 
 def run_tests_and_create_report(report_name, *tests, **kwargs):
     import spyne.test
     import pytest
-    from glob import glob
-    from itertools import chain
 
     if os.path.isfile(report_name):
         os.unlink(report_name)
@@ -117,6 +117,7 @@ def call_pytest(*tests, **kwargs):
 def call_pytest_subprocess(*tests, **kwargs):
     global _ctr
     import pytest
+
     _ctr += 1
     file_name = 'test_result.%d.xml' % _ctr
     if os.path.isfile(file_name):
@@ -125,10 +126,9 @@ def call_pytest_subprocess(*tests, **kwargs):
     args.extend('--{0}={1}'.format(k, v) for k, v in kwargs.items())
     return call_test(pytest.main, args, tests)
 
+
 def call_trial(*tests, **kwargs):
     import spyne.test
-    from glob import glob
-    from itertools import chain
 
     global _ctr
     _ctr += 1
@@ -158,10 +158,6 @@ def call_trial(*tests, **kwargs):
     return int(not test_result.wasSuccessful())
 
 
-class InstallTestDeps(TestCommand):
-    pass
-
-
 def subunit2junitxml(ctr):
     from testtools import ExtendedToStreamDecorator
     from testtools import StreamToExtendedDecorator
@@ -179,7 +175,8 @@ def subunit2junitxml(ctr):
 
     subunit2 = BytesIO()
     run_tests_from_stream(open(subunit1_file_name, 'rb'),
-                    ExtendedToStreamDecorator(StreamResultToBytes(subunit2)))
+                          ExtendedToStreamDecorator(
+                              StreamResultToBytes(subunit2)))
     subunit2.seek(0)
 
     sys.argv = ['subunit2junitxml']
@@ -191,161 +188,13 @@ def subunit2junitxml(ctr):
     junit_file_name = 'test_result.%d.xml' % ctr
 
     filter_by_result(f, junit_file_name, True, False, protocol_version=2,
-                                passthrough_subunit=True, input_stream=subunit2)
-
-
-class ExtendedTestCommand(TestCommand):
-    """TestCommand customized to project needs."""
-
-    user_options = TestCommand.user_options + [
-        ('capture=', 'k', "py.test output capture control (see py.test "
-         "--capture)"),
-    ]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.capture = 'fd'
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-
-        self.test_args = []
-        self.test_suite = True
-
-
-class RunTests(ExtendedTestCommand):
-    def run_tests(self):
-        print("running tests")
-        sys.path.append(join(EXAMPLES_DIR, 'django'))
-        os.environ['DJANGO_SETTINGS_MODULE'] = 'rpctest.settings'
-        ret = 0
-        ret = call_pytest('interface', 'model', 'multipython', 'protocol',
-                          'test_null_server.py', 'test_service.py',
-                          'test_soft_validation.py', 'test_util.py',
-                          'test_sqlalchemy.py',
-                          'test_sqlalchemy_deprecated.py',
-                          # here we run django tests in the same process
-                          # for coverage reason
-                          'interop/test_django.py',
-                          'interop/test_pyramid.py', capture=self.capture) or ret
-        # test different versions of Django
-        # FIXME: better to use tox in CI script
-        # For now we run it here
-        from tox._config import parseconfig
-        from tox._cmdline import Session
-        tox_args = ['-ctox.django.ini']
-        config = parseconfig(tox_args, 'tox')
-        ret = Session(config).runcommand() or ret
-
-        ret = call_pytest_subprocess('interop/test_httprpc.py',
-                                     capture=self.capture) or ret
-        ret = call_pytest_subprocess('interop/test_soap_client_http.py',
-                                     capture=self.capture) or ret
-        ret = call_pytest_subprocess('interop/test_soap_client_zeromq.py',
-                                     capture=self.capture) or ret
-        ret = call_pytest_subprocess('interop/test_suds.py',
-                                     capture=self.capture) or ret
-        ret = call_trial('interop/test_soap_client_http_twisted.py',
-                         'transport/test_msgpack.py', capture=self.capture) or ret
-
-        if ret == 0:
-            print(GREEN + "All that glisters is not gold." + RESET)
-        else:
-            print(RED + "Something is rotten in the state of Denmark." + RESET)
-
-        raise SystemExit(ret)
-
-
-class RunDjangoTests(ExtendedTestCommand):
-
-    """Run django interoperability tests.
-
-    Useful for Tox.
-
-    """
-
-    def run_tests(self):
-        import django
-        print("running django tests")
-        sys.path.append(join(EXAMPLES_DIR, 'django'))
-        os.environ['DJANGO_SETTINGS_MODULE'] = 'rpctest.settings'
-        file_name = 'test_result_django_{0}.xml'.format(django.get_version())
-        ret = run_tests_and_create_report(file_name, 'interop/test_django.py',
-                                          capture=self.capture)
-
-        if ret == 0:
-            print(GREEN + "All Django tests passed." + RESET)
-        else:
-            print(RED + "At least one Django test failed." + RESET)
-
-        raise SystemExit(ret)
-
-
-class RunMultiPythonTests(TestCommand):
-    """Run tests compatible with different python implementations. """
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-
-        self.test_args = []
-        self.test_suite = True
-
-    def get_python_flavour(self):
-        try:
-            # CPython2, PyPy
-            return sys.subversion[0]
-        except AttributeError:
-            pass
-
-        try:
-            # CPython3
-            return sys.implementation.cache_tag
-        except AttributeError:
-            pass
-
-        try:
-            # Jython
-            return sys.JYTHON_JAR
-        except AttributeError:
-            pass
-
-        raise NotImplementedError
-
-    def run_tests(self):
-        flavour = self.get_python_flavour()
-        file_name = 'test_result.multi_python_{0}.xml'.format(flavour)
-        ret = run_tests_and_create_report(file_name, 'multipython')
-
-        if ret == 0:
-            print(GREEN + "All multi Python tests passed." + RESET)
-        else:
-            print(RED + "At least one multi Python test failed." + RESET)
-
-        raise SystemExit(ret)
-
-
-multi_python_test_reqs = ['pytest', 'coverage', 'junitxml']
-
-if 'test_multi_python' in sys.argv:
-    test_reqs = multi_python_test_reqs
-else:
-    test_reqs = multi_python_test_reqs + [
-        'pytest', 'werkzeug', 'sqlalchemy',
-        'lxml>=2.3', 'pyyaml', 'pyzmq', 'twisted', 'colorama',
-        'msgpack-python', 'webtest', 'django', 'pytest_django',
-        'python-subunit', 'pyramid',
-        'tox'
-    ]
-
-    if sys.version_info < (3,0):
-        test_reqs.extend(['pyparsing<1.99', 'suds'])
-    else:
-        test_reqs.extend(['pyparsing', 'suds-jurko'])
+                     passthrough_subunit=True, input_stream=subunit2)
 
 
 class SubUnitTee(object):
     def __init__(self, name):
         self.name = name
+
     def __enter__(self):
         self.file = open(self.name, 'wb')
         self.stdout = sys.stdout
@@ -376,12 +225,89 @@ class SubUnitTee(object):
 
         self.stdout.write(data)
 
-    def read(self,d=0):
+    def read(self, d=0):
         return ''
 
     def flush(self):
         self.stdout.flush()
         self.stderr.flush()
+
+
+class ExtendedTestCommand(TestCommand):
+    """TestCommand customized to project needs."""
+
+    user_options = TestCommand.user_options + [
+        ('capture=', 'k', "py.test output capture control (see py.test "
+                          "--capture)"),
+    ]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.capture = 'fd'
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+
+        self.test_args = []
+        self.test_suite = True
+
+
+class RunTests(ExtendedTestCommand):
+    def run_tests(self):
+        print("running tests")
+        ret = 0
+        ret = call_pytest('interface', 'model', 'multipython', 'protocol',
+                          'test_null_server.py', 'test_service.py',
+                          'test_soft_validation.py', 'test_util.py',
+                          'test_sqlalchemy.py',
+                          'test_sqlalchemy_deprecated.py',
+                          'interop/test_pyramid.py',
+                          capture=self.capture) or ret
+
+        ret = call_pytest_subprocess('interop/test_httprpc.py',
+                                     capture=self.capture) or ret
+        ret = call_pytest_subprocess('interop/test_soap_client_http.py',
+                                     capture=self.capture) or ret
+        ret = call_pytest_subprocess('interop/test_soap_client_zeromq.py',
+                                     capture=self.capture) or ret
+        ret = call_pytest_subprocess('interop/test_suds.py',
+                                     capture=self.capture) or ret
+        ret = call_trial('interop/test_soap_client_http_twisted.py',
+                         'transport/test_msgpack.py',
+                         capture=self.capture) or ret
+
+        if ret == 0:
+            print(GREEN + "All that glisters is not gold." + RESET)
+        else:
+            print(RED + "Something is rotten in the state of Denmark." + RESET)
+
+        raise SystemExit(ret)
+
+
+class RunPython3Tests(TestCommand):
+    """Run tests compatible with different python implementations. """
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        file_name = 'test_result_py3.xml'
+        ret = run_tests_and_create_report(file_name,
+                                          'multipython',
+                                          'model/test_enum.py',
+                                          'model/test_exception.py',
+                                          'model/test_include.py',
+                                          )
+
+        if ret == 0:
+            print(GREEN + "All Python 3 tests passed." + RESET)
+        else:
+            print(RED + "At one Python 3 test failed." + RESET)
+
+        raise SystemExit(ret)
 
 # Testing stuff ends here.
 ###############################
@@ -430,9 +356,7 @@ setup(
         ]
     },
 
-    tests_require = test_reqs,
-    cmdclass = {'test': RunTests, 'install_test_deps': InstallTestDeps,
-                'test_django': RunDjangoTests,
-                'test_multi_python': RunMultiPythonTests
+    cmdclass = {'test': RunTests,
+                'test_python3': RunPython3Tests
                },
 )
