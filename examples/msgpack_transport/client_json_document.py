ea035558572bd30589152247bd6bc4504455052f
Burak Arslan <burak.arslan@arskom.com.tr>
2014-08-07 13:28:17 +0300

    msgpack transport examples.

---
+++ examples/msgpack_transport/client_json_document.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# encoding: utf8
+#
+# Copyright © Burak Arslan <burak at arskom dot com dot tr>,
+#             Arskom Ltd. http://www.arskom.com.tr
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    1. Redistributions of source code must retain the above copyright notice,
+#       this list of conditions and the following disclaimer.
+#    2. Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#    3. Neither the name of the owner nor the names of its contributors may be
+#       used to endorse or promote products derived from this software without
+#       specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+"""
+Raw socket client example for MessagePackDocument via MessagePack server.
+"""
+
+from __future__ import print_function, absolute_import
+
+# These are analogues from spyne.server.msgpack. What's IN_REQUEST there is
+# OUT_REQUEST here because an outgoing request from a client's perspective is an
+# incoming requests from a server's perspective.
+IN_RESPONSE_NO_ERROR = 0
+IN_RESPONSE_CLIENT_ERROR = 1
+IN_RESPONSE_SERVER_ERROR = 2
+
+OUT_REQUEST = 1
+
+
+import socket
+
+import json
+import msgpack
+
+from spyne.util.six import BytesIO
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("localhost", 5551))
+
+request_document = {"say_hello": ["Dave", 5]}
+
+# Because the server's input protocol is JsonDocument, we serialize the
+# request document to json bytestream.
+request_bytestream = json.dumps(request_document)
+
+# Because our server side transport is msgpack, we put the request bytestream
+# inside a MessagePack document.
+request_wrapper_document = [OUT_REQUEST, request_bytestream]
+
+# and we serialize the request wrapper document to msgpack bytestream as well
+request_wrapper_bytestream = msgpack.packb(request_wrapper_document)
+
+# which we push to the socket.
+s.sendall(request_wrapper_bytestream)
+
+in_buffer = msgpack.Unpacker()
+while True:
+    in_buffer.feed(s.recv(1))
+    for msg in in_buffer:
+        print("Response document:", msg)
+        for resp_code, data in msg.items():
+            if resp_code == IN_RESPONSE_NO_ERROR:
+                print("Success. Response: ", json.loads(data))
+
+            elif resp_code == IN_RESPONSE_CLIENT_ERROR:
+                print("Invalid Request. Details: ", json.loads(data))
+
+            elif resp_code == IN_RESPONSE_SERVER_ERROR:
+                print("Internal Error. Details: ", json.loads(data))
+
+            else:
+                print ("Unknown response. Update the client. "
+                       "Additional data:", data)
+        # As we only sent one request, we must break after
+        # receiving its response.
+        break
+    else:
+        continue
+
+    break  # break after receiving one message
---
+++ examples/msgpack_transport/server_msgpack_document.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# encoding: utf8
+#
+# Copyright © Burak Arslan <burak at arskom dot com dot tr>,
+#             Arskom Ltd. http://www.arskom.com.tr
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    1. Redistributions of source code must retain the above copyright notice,
+#       this list of conditions and the following disclaimer.
+#    2. Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#    3. Neither the name of the owner nor the names of its contributors may be
+#       used to endorse or promote products derived from this software without
+#       specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+"""
+This is a simple HelloWorld example to show the basics of writing a
+MessagePack-via-vanilla-TCP-sockets server.
+"""
+
+
+import logging
+
+from spyne import Application, rpc, ServiceBase, Iterable, UnsignedInteger, \
+    Unicode
+
+from spyne.protocol.msgpack import MessagePackDocument
+from spyne.server.twisted.msgpack import TwistedMessagePackProtocolFactory
+from spyne.server.wsgi import WsgiApplication
+
+
+HOST = '127.0.0.1'
+PORT = 5550
+
+
+class HelloWorldService(ServiceBase):
+    @rpc(Unicode, UnsignedInteger, _returns=Iterable(Unicode))
+    def say_hello(ctx, name, times):
+        for i in range(times):
+            yield u'Hello, %s' % name
+
+
+if __name__=='__main__':
+    from twisted.internet import reactor
+    from twisted.python import log
+
+    # logging boilerplate
+    logging.basicConfig(level=logging.DEBUG)
+    logging.getLogger('spyne.protocol.xml').setLevel(logging.DEBUG)
+
+    observer = log.PythonLoggingObserver('twisted')
+    log.startLoggingWithObserver(observer.emit, setStdout=False)
+
+    # set up application
+    application = Application([HelloWorldService],
+        'spyne.examples.hello.msgpack', in_protocol=MessagePackDocument(),
+                                            out_protocol=MessagePackDocument())
+
+    # set up transport
+    factory = TwistedMessagePackProtocolFactory(application)
+
+    # set up listening endpoint
+    reactor.listenTCP(PORT, factory, interface=HOST)
+
+    # run the reactor
+    reactor.run()
