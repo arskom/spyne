57e2f98dcc7d6a184f800fa5406513e88ec0ef1f
Burak Arslan <burak.arslan@arskom.com.tr>
2014-10-14 19:59:38 +0300

    use a better invalid input for msgpack's invalid_input test.

--- spyne/test/protocol/test_msgpack.py
+++ spyne/test/protocol/test_msgpack.py
@@ -59,7 +59,7 @@ class TestMessagePackRpc(unittest.TestCase):
         server = ServerBase(app)
 
         initial_ctx = MethodContext(server)
-        initial_ctx.in_string = ['{']
+        initial_ctx.in_string = ['\xdf'] # Invalid input
         ctx, = server.generate_contexts(initial_ctx)
         assert ctx.in_error.faultcode == 'Client.MessagePackDecodeError'
 
