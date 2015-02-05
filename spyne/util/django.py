96f763ad9b706910f08e7751b9e4cde4d6ef6d99
Rinat Shigapov <rinatshigapov@gmail.com>
2015-02-05 20:53:12 +0300

    removed needless braces

--- spyne/util/django.py
+++ spyne/util/django.py
@@ -55,7 +55,7 @@ email_re = re.compile(
     # or have at least two symbols
     # hyphen can't be at the beginning or end of domain part
     # domain should contain at least 2 parts, the last one is TLD
-    r"(([a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])+))\.)+"
+    r"([a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])+)\.)+"
     # TLD should contain only letters, at least 2
     r"[A-Za-z]{2,}", re.IGNORECASE)
 
