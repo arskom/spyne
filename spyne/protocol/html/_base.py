331c6d9d96bab2e8d43a676ceb7db9446586db66
Burak Arslan <burak.arslan@arskom.com.tr>
2015-03-06 10:58:03 +0200

    fix parse_file in HtmlBase

--- spyne/protocol/html/_base.py
+++ spyne/protocol/html/_base.py
@@ -41,8 +41,8 @@ class HtmlBase(XmlCloth):
         if cloth_parser is None:
             cloth_parser = html.HTMLParser(remove_comments=True)
 
-        self._cloth = html.parse(self._cloth, parser=cloth_parser)
-        self._cloth = self._cloth.getroot()
+        cloth = html.parse(file_name, parser=cloth_parser)
+        return cloth.getroot()
 
     def docfile(self, *args, **kwargs):
         return etree.htmlfile(*args, **kwargs)
