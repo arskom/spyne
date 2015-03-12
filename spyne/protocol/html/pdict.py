53940472e92be3b2dc9b126fa08d24bfccd917d0
Burak Arslan <burak.arslan@arskom.com.tr>
2015-03-12 02:24:53 +0200

    Implement pretty dict protocol

---
+++ spyne/protocol/html/pdict.py
@@ -0,0 +1,28 @@
+# encoding: utf8
+#
+# spyne - Copyright (C) Spyne contributors.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+#
+
+from lxml.builder import E
+from pprint import pformat
+
+from spyne.protocol.html import HtmlBase
+
+
+class PrettyDict(HtmlBase):
+    def to_parent(self, ctx, cls, inst, parent, name, **kwargs):
+        parent.write(E.pre(pformat(inst)))
