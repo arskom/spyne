a7ad3dc5fc81bc0f19872ea528b133e3cef2f931
Burak Arslan <burak.arslan@arskom.com.tr>
2015-03-12 23:38:38 +0200

    remove identical overriding function

--- spyne/protocol/xml.py
+++ spyne/protocol/xml.py
@@ -286,9 +286,6 @@ class XmlDocument(SubXmlBase):
             encoding=encoding,
         )
 
-    def subserialize(self, ctx, cls, inst, parent, ns=None, name=None):
-        return self.to_parent(ctx, cls, inst, parent, name)
-
     def set_validator(self, validator):
         if validator in ('lxml', 'schema') or \
                                     validator is self.SCHEMA_VALIDATION:
