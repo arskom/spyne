48a89b4cdb2e084f4b349e9ca12a91daba9e0734
Sunpoet Po-Chuan Hsieh <sunpoet@sunpoet.net>
2015-12-08 13:35:43 +0800

    Fix build with Python 3

--- spyne/test/transport/test_msgpack.py
+++ spyne/test/transport/test_msgpack.py
@@ -54,9 +54,9 @@ class TestMessagePackServer(unittest.TestCase):
         request = msgpack.packb({'yay': [v]})
         prot.dataReceived(msgpack.packb([1, request]))
         val = prot.transport.value()
-        print repr(val)
+        print(repr(val))
         val = msgpack.unpackb(val)
-        print repr(val)
+        print(repr(val))
 
         self.assertEquals(val, [0, msgpack.packb(v)])
 
@@ -82,9 +82,9 @@ class TestMessagePackServer(unittest.TestCase):
         request = msgpack.packb({'yay': [v]})
         def _ccb(_):
             val = prot.transport.value()
-            print repr(val)
+            print(repr(val))
             val = msgpack.unpackb(val)
-            print repr(val)
+            print(repr(val))
 
             self.assertEquals(val, [0, msgpack.packb(v)])
 
