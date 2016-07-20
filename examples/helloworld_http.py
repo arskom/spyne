6ae79e01e48e3becad83ddb4a51dc174557358da
Burak Arslan <burak.arslan@arskom.com.tr>
2016-07-20 14:50:02 +0300

    minor

--- examples/helloworld_http.py
+++ examples/helloworld_http.py
@@ -65,9 +65,10 @@ class HelloWorldService(ServiceBase):
             yield 'Hello, %s' % name
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     # Python daemon boilerplate
     from wsgiref.simple_server import make_server
+
     logging.basicConfig(level=logging.DEBUG)
 
     # Instantiate the application by giving it:
@@ -76,19 +77,19 @@ if __name__=='__main__':
     #   * An input protocol.
     #   * An output protocol.
     application = Application([HelloWorldService], 'spyne.examples.hello.http',
-          # The input protocol is set as HttpRpc to make our service easy to
-          # call. Input validation via the 'soft' engine is enabled. (which is
-          # actually the the only validation method for HttpRpc.)
-          in_protocol=HttpRpc(validator='soft'),
+        # The input protocol is set as HttpRpc to make our service easy to
+        # call. Input validation via the 'soft' engine is enabled. (which is
+        # actually the the only validation method for HttpRpc.)
+        in_protocol=HttpRpc(validator='soft'),
 
-          # The ignore_wrappers parameter to JsonDocument simplifies the reponse
-          # dict by skipping outer response structures that are redundant when
-          # the client knows what object to expect.
-          out_protocol=JsonDocument(ignore_wrappers=True),
-      )
+        # The ignore_wrappers parameter to JsonDocument simplifies the reponse
+        # dict by skipping outer response structures that are redundant when
+        # the client knows what object to expect.
+        out_protocol=JsonDocument(ignore_wrappers=True),
+    )
 
     # Now that we have our application, we must wrap it inside a transport.
-    # In this case, we use Spyne's standard Wsgi wrapper. Spyne supports 
+    # In this case, we use Spyne's standard Wsgi wrapper. Spyne supports
     # popular Http wrappers like Twisted, Django, Pyramid, etc. as well as
     # a ZeroMQ (REQ/REP) wrapper.
     wsgi_application = WsgiApplication(application)
