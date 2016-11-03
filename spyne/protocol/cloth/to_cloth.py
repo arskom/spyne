1948044ba8fc7f9d3d217d6df87ea5120001a4a8
Burak Arslan <burak.arslan@arskom.com.tr>
2016-11-03 15:54:43 +0300

    nicer log messages for cloth

--- spyne/protocol/cloth/to_cloth.py
+++ spyne/protocol/cloth/to_cloth.py
@@ -693,11 +693,11 @@ class ToClothMixin(OutProtocolBase, ClothParserMixin):
 
         if len(fti_check) > 0:
             if len(fti_check) > 0:
-                logger_s.debug("Skipping the following: %r",
+                logger_s.debug("No element found for the following fields: %r",
                                                          list(fti_check.keys()))
             if len(never_found) > 0:
-                logger_s.debug("Never found the following: %r",
-                                                              list(never_found))
+                logger_s.debug("No field found for element the following "
+                               "elements: %r", list(never_found))
 
     @coroutine
     def array_to_cloth(self, ctx, cls, inst, cloth, parent, name=None, **kwargs):
