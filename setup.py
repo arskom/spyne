700ddf123667b32c499e5693694df20517b56271
Burak Arslan <burak.arslan@arskom.com.tr>
2017-06-02 18:29:37 +0300

    fixes #532

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ protocols and transports.
 
 try:
     os.stat('CHANGELOG.rst')
-    LONG_DESC += u"\n\n" + open('CHANGELOG.rst', 'r').read().decode('utf8')
+    LONG_DESC += u"\n\n" + open('CHANGELOG.rst', 'rb').read().decode('utf8')
 except OSError:
     pass
 
