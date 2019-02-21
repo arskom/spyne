15cb3cfd83f7dbf98266ea87253c6d8a8d884b48
Burak Arslan <burak.arslan@arskom.com.tr>
2019-02-21 15:40:17 +0300

    request.sh comment

--- examples/xml/soap12-mtom/request.sh
+++ examples/xml/soap12-mtom/request.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -x
 
+# The "boundary" string here must match with the one in the request body
+# (typically the first line of the request body)
+
+# The "start" string is similarly the content id of the first mime part
+# it is currently ignored by the MtoM parser
+
 curl -X POST \
     -H 'Content-Type: multipart/related; type="application/xop+xml"; '`
        `'boundary="uuid:2e53e161-b47f-444a-b594-eb6b72e76997"; '`
