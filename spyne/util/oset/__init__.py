8c33df3d88460b591e5f4295bbacec04c8fdb50d
Burak Arslan <burak.arslan@arskom.com.tr>
2020-02-18 14:52:47 +0300

    Remove old oset. It was for Python<=2.6 compat.

--- spyne/util/oset/__init__.py
+++
@@ -1,9 +0,0 @@
-
-"""The ``spyne.util.oset`` package contains implementation of an ordered set
-that works on Python versions 2.4 through 2.7.
-"""
-
-try:
-    from spyne.util.oset.new import oset
-except (SyntaxError, ImportError, AttributeError):
-    from spyne.util.oset.old import oset
