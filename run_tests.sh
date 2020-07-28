3fbf51c4ee18fe1f8ee5da65bf89066f7ec739b2
Burak Arslan <burak.arslan@arskom.com.tr>
2020-07-28 19:04:05 +0300

    verbump

--- run_tests.sh
+++ run_tests.sh
@@ -62,9 +62,9 @@ if [ -z "$FN" ]; then
     declare -A URLS;
     URLS["cpy27"]="2.7.17/Python-2.7.17.tar.xz";
     URLS["cpy35"]="3.5.9/Python-3.5.9.tar.xz";
-    URLS["cpy36"]="3.6.10/Python-3.6.10.tar.xz";
-    URLS["cpy37"]="3.7.6/Python-3.7.6.tar.xz";
-    URLS["cpy38"]="3.8.1/Python-3.8.1.tar.xz";
+    URLS["cpy36"]="3.6.11/Python-3.6.11.tar.xz";
+    URLS["cpy37"]="3.7.8/Python-3.7.8.tar.xz";
+    URLS["cpy38"]="3.8.5/Python-3.8.5.tar.xz";
     URLS["jyt27"]="2.7.1/jython-installer-2.7.1.jar";
     URLS["ipy27"]="ipy-2.7.4.zip";
 
